2666b3486372f377dbd3ea39d0c4abf51415d852
Pierre Ablin <pierreablin@gmail.com>
2021-02-28 19:39:34 +0100

    change version number

--- qndiag/__init__.py
+++ qndiag/__init__.py
@@ -19,7 +19,7 @@
 # Dev branch marker is: 'X.Y.devN' where N is an integer.
 #
 
-__version__ = '0.1.dev0'
+__version__ = '0.1'
 
 from .qndiag import qndiag, transform_set, loss, gradient  # noqa
 from .pham import ajd_pham  # noqa
